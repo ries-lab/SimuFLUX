1bacad4c485d71ed122f59355a8a66c81917feb0
Jonas Ries <ries@embl.de>
2025-02-04 13:03:05 +0100

    dna paint

--- examples/example9_DNAPAINT_imaging.m
+++ examples/example9_DNAPAINT_imaging.m
@@ -27,7 +27,7 @@ laserpower=5;
 switchpar.brightness=100*laserpower;
 switchpar.toffsmlm=5*1e3; %on-switching time in ms
 switchpar.photonbudget=5000;
-switchpar.tonsmlm=100; % ms stays on, only bleached
+switchpar.tonsmlm=100; % ms 
 switchpar.activations=5; %re activations
 switchpar.starton=0; %fluorophores start in random on / off state, determined by tonsmlm, toffsmlm
 fc.setpar(switchpar)
@@ -35,14 +35,12 @@ fc.setpar(switchpar)
 %add fake NPCs
 fc.add(posnpc);
 
-% make diffusing molecules
-% standard sequences: ~1 nM
-% fast sequences: 0.2 pM
-% 1 M = Na/liter 
-% 1 l= (0.1 m)^3 = (0.1 *1e6 um)^3 =1e15 um^3
-% c1nM= 1e-9* 6e23/1e15
-% bounding box: 1 um  x 1 um x 2 um: ~ 1 particle
-% fast sequences 2 um x 2 um x um: ~1 particle
+% make diffusing molecules, back of the envelope calculations
+% standard sequences: ~1 nM, fast sequences: 0.2 pM
+% 1 M = Na/liter, 1 l= (0.1 m)^3 = (0.1 *1e6 um)^3 =1e15 um^3
+% c1nM= 1e-9* 6e23/1e15 %density of fluorophores, about 1 / um^3
+% slow sequences, bounding box: 1 um  x 1 um x 2 um: ~ 1 particle
+% fast sequences: 2 um x 2 um x um: ~1 particle
 D=10; %um^2/s
 fd=FlMoving;
 
@@ -50,28 +48,25 @@ fd.makediffusion(D,0.01,dim=3,boundarybox=[500 500 1000]);
 fc.add(fd)
 
 sim.fluorophores=fc;
+repetitions=100;
+brightnesses=[0,1]; %compare without and with background from diffusing fluorophore
 
-brightnesses=[1, 0]; %compare without and with background from diffusing fluorophore
 for br=1:length(brightnesses)
-fd.brightness=switchpar.brightness*brightnesses(br);
-
-out=sim.scoutingSequence(maxrep=100);
-
-%plot results
-vld=out.loc.vld==1 & out.loc.itr==max(out.loc.itr) ;
-vldcfr=vld & out.loc.cfr<0.1;
-figure(89+br); hold off;
-posdiff=squeeze(out.fluorophores.pos(:,end,:));
-% plot(posdiff(:,1),posdiff(:,2),'c')
-hold on
-plot(sim.scoutingcoordinates(:,1),sim.scoutingcoordinates(:,2),'k+')
-hold on
-plot(out.loc.xnm(vld),out.loc.ynm(vld),'r.')
-plot(out.loc.xnm(vldcfr),out.loc.ynm(vldcfr),'bx')
-posfl=out.fluorophores.pos(end,:,2:end);
-plot(posfl(:,1),posfl(:,2),'mo')
-
-
-axis equal
-legend('scouting','last itr vld','last itr vld +cfr', 'fluorophore')
+    sim.posgalvo=[0 0 0];sim.posEOD=[0 0 0];sim.time=0;
+    fd.brightness=switchpar.brightness*brightnesses(br);
+    
+    out=sim.scoutingSequence(maxrep=repetitions);
+    
+    %plot results
+    vld=out.loc.vld==1 & out.loc.itr==max(out.loc.itr) ;
+    vldcfr=vld & out.loc.cfr<0.1;
+    figure(89+br); hold off;
+    plot(sim.scoutingcoordinates(:,1),sim.scoutingcoordinates(:,2),'k+')
+    hold on
+    plot(out.loc.xnm(vld),out.loc.ynm(vld),'r.')
+    plot(out.loc.xnm(vldcfr),out.loc.ynm(vldcfr),'bx')
+    posfl=out.fluorophores.pos(end,:,2:end);
+    plot(posfl(:,1),posfl(:,2),'mo')
+    axis equal
+    legend('scouting','last itr vld','last itr vld +cfr', 'fluorophore')
 end
