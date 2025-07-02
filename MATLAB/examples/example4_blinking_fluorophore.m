e3e7de08a001f923c64888fe16439e1ec29e13d7
Jonas Ries <ries@embl.de>
2025-07-02 09:24:11 +0200

    flickering

--- MATLAB/examples/example4_blinking_fluorophore.m
+++ MATLAB/examples/example4_blinking_fluorophore.m
@@ -15,7 +15,7 @@ orbitpoints=4;
 probecenter=true;
 psf_donut=PsfDonut2D;
 sim.definePattern("donut", psf_donut, makepattern="orbitscan", orbitpoints=orbitpoints, ...
-    probecenter=probecenter,orbitL=L,pointdwelltime=pointdwelltime,laserpower=100,...
+    probecenter=probecenter,orbitL=L,pointdwelltime=pointdwelltime,laserpower=30,...
     repetitions=repetitions);
 % sim.defineComponent("estdonut","estimator",@est_donut2d,parameters={sim.patterns("donut").pos,L,360},dim=1:2);
 sim.defineComponent("estsq","estimator",@est_qLSQiter2D,parameters={L,probecenter},dim=1:2);
@@ -24,27 +24,40 @@ out=sim.runSequence({"donut","estsq"});sim.summarize_results(out);
 %% plot std vs repetitions
 %Fig. 2
 allrepetitions=1:1:25;
-stdx=zeros(length(allrepetitions),1);stdy=stdx;stdxrel=stdx;stdyrel=stdy;biasx=stdy;biasy=stdy;
+laserpowers=[10 100 100000]/0.88;
 
-for k=1:length(allrepetitions)
-    pointdwelltime=pointdwelltimerep/allrepetitions(k);%us
-    sim.definePattern("donut4", psf_donut, makepattern="orbitscan", orbitpoints=orbitpoints, ...
-    probecenter=probecenter,orbitL=L,pointdwelltime=pointdwelltime,laserpower=100,repetitions=allrepetitions(k));
-    out=sim.runSequence({"donut4","estsq"},maxlocs=3000);
-    bright=out.loc.phot>quantile(out.loc.phot,0.1); %filter out localizations that are too dim, outliers from fluorophores that are mostly off
-    converged=abs(out.loc.xnm)<L & abs(out.loc.ynm)<L; %estimator is artificially bound by +/-L. Here we remove localizations where the estimator converged to this limit.
-    sr=sim.summarize_results(out,filter=bright&converged,display=false);
-    stdx(k)=sr.std(1);
-    stdy(k)=sr.std(2);
-    % crb=sr.sCRB(1);
-    stdxrel(k)=stdx(k)/sr.sCRB(1);
-    stdyrel(k)=stdy(k)/sr.sCRB(2);
-    biasx(k)=sr.bias(1);
-    biasy(k)=sr.bias(2);
+
+clear results
+for l=1:length(laserpowers)
+    for k=1:length(allrepetitions)
+        pointdwelltime=pointdwelltimerep/allrepetitions(k);%us
+        sim.definePattern("donut4", psf_donut, makepattern="orbitscan", orbitpoints=orbitpoints, ...
+        probecenter=probecenter,orbitL=L,pointdwelltime=pointdwelltime,laserpower=laserpowers(l),repetitions=allrepetitions(k));
+        out=sim.runSequence({"donut4","estsq"},maxlocs=3000);
+        bright=out.loc.phot>quantile(out.loc.phot,0.1); %filter out localizations that are too dim, outliers from fluorophores that are mostly off
+        converged=abs(out.loc.xnm)<L & abs(out.loc.ynm)<L; %estimator is artificially bound by +/-L. Here we remove localizations where the estimator converged to this limit.
+        sr=sim.summarize_results(out,filter=bright&converged,display=false);
+        results(l).stdx(k)=sr.std(1);
+        results(l).stdy(k)=sr.std(2);
+        % crb=sr.sCRB(1);
+        results(l).stdxrel(k)=stdx(k)/sr.sCRB(1);
+        results(l).stdyrel(k)=stdy(k)/sr.sCRB(2);
+        results(l).biasx(k)=sr.bias(1);
+        results(l).biasy(k)=sr.bias(2);
+        results(l).crb(k)=sr.sCRB(1);
+        results(l).phot(k)=sr.phot;
+    end
 end
+%%
 figure(240)
-plot(allrepetitions,stdxrel,allrepetitions,stdyrel,allrepetitions,allrepetitions*0+1,'k')
+hold off
+plot(allrepetitions,results(1).stdx,'rx',allrepetitions,results(1).crb,'r--')
+hold on
+plot(allrepetitions,results(2).stdx,'bo',allrepetitions,results(2).crb,'b--')
+plot(allrepetitions,results(3).stdx,'k',allrepetitions, sqrt(results(3).stdx.^2+results(1).crb.^2),'r',allrepetitions, sqrt(results(3).stdx.^2+results(2).crb.^2),'b')
+
 xlabel('repetitions')
-ylabel('std/sCRB')
+ylabel('std (nm)')
 title("flickering fluorophores, mitigation by repetitive scanning")
+legend("N="+round(results(1).phot(1)), "CRB", "N="+round(results(2).phot(1)), "CRB", "N high", "prediction", "prediction")
 
